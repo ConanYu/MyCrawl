f225f12d346e7b2614866acb2bfbf57a28553f5f
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-10-13 19:54:22 +0800

    Update getlist_organization.py

--- simulate/codeforces/getlist_organization.py
+++ simulate/codeforces/getlist_organization.py
@@ -5,9 +5,8 @@ def getlist_organization(idx):
     url = 'http://codeforces.com/ratings/organization/' + str(idx)
     response = requests.get(url)
     html = etree.HTML(response.text)
-    result = html.xpath('//*[@class="datatable ratingsDatatable"]//a/@href')
-    v = [e[9:] for e in result]
-    return v
+    result = html.xpath('//div[@class="datatable ratingsDatatable"]//a/@href')
+    return [e[9:] for e in result]
 
 if __name__ == '__main__':
     print(getlist_organization(750))
