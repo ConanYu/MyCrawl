ed40e65eb471e602b3e00a603a7cf1bdc450d8b7
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-10-13 17:11:01 +0800

    Update submit.py

--- simulate/GzhuOJ/submit.py
+++ simulate/GzhuOJ/submit.py
@@ -1,42 +1,40 @@
 import os
 import time
 import requests
-from lxml import etree
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
-robot = webdriver.Chrome(executable_path=r'D:\Desktop\chromedriver.exe')
-session = requests.Session()
-submit_data = {'Problem': '1000', 'Language': 2, 'Code': ''}
-# head
+robot, session = None, None
 
-def write(name, data, path=None):
-    PATH = os.path.abspath(os.path.dirname(__file__))
-    with open(PATH + '\\' + name, 'w', encoding='utf-8', newline='') as f:
-        f.write(data)
-
-def operator_login():
-    global robot
-    robot.find_element_by_xpath('//*[@id="login"]').click()
-    robot.find_element_by_xpath('//*[@id="login_username"]').send_keys('Your Username')
-    robot.find_element_by_xpath('//*[@id="login_password"]').send_keys('Your Password')
-    robot.find_element_by_xpath('//*[@id="login_submit"]').click()
-
-def operator_submit():
-    global robot, submit_data
-    robot.find_element_by_xpath('//*[@id="pid"]').send_keys(submit_data['Problem'])
-    Select(robot.find_element_by_xpath('//*[@id="lang"]')).select_by_value('2')
-    robot.find_element_by_xpath('//*[@id="code"]').send_keys(submit_data['Code'])
-    robot.find_element_by_xpath('//*[@id="submit"]').click()
-
-def todo(url, operator):
+def todo(url, operator, **kw):
     global robot, session
+    if robot is None or session is None:
+        robot = webdriver.Chrome(executable_path=r'D:\Desktop\chromedriver.exe')
+        session = requests.Session()
     robot.get(url)
-    operator()
+    operator(kw)
     cookies = robot.get_cookies()
     for cookie in cookies:
         session.cookies.set(cookie['name'], cookie['value'])
 
-submit_data['Code'] = r'''
+# ************************************************** HEAD ************************************************** #
+
+def operator_login(kw):
+    # url: http://172.22.27.1/
+    global robot
+    robot.find_element_by_xpath('//*[@id="login"]').click()
+    robot.find_element_by_xpath('//*[@id="login_username"]').send_keys(kw['username'])
+    robot.find_element_by_xpath('//*[@id="login_password"]').send_keys(kw['password'])
+    robot.find_element_by_xpath('//*[@id="login_submit"]').click()
+
+def operator_submit(kw):
+    # url: http://172.22.27.1/submit
+    global robot
+    robot.find_element_by_xpath('//*[@id="pid"]').send_keys(kw['problem'])
+    Select(robot.find_element_by_xpath('//*[@id="lang"]')).select_by_value(kw['lang'])
+    robot.find_element_by_xpath('//*[@id="code"]').send_keys(kw['code'])
+    robot.find_element_by_xpath('//*[@id="submit"]').click()
+
+CODE = r'''
 #include<iostream>
 using namespace std;
 int main()
@@ -50,7 +48,7 @@ int main()
 '''
 
 if __name__ == '__main__':
-    todo('http://172.22.27.1/', operator_login)
-    for i in range(5):
-        todo('http://172.22.27.1/submit', operator_submit)
-        time.sleep(0.5) # 太快了，防卡壳
+    todo('http://172.22.27.1', operator_login, username='1706300092', password='12345679')
+    for i in range(2):
+        todo('http://172.22.27.1/submit', operator_submit, problem='1000', lang='2', code=CODE)
+        time.sleep(0.5)
