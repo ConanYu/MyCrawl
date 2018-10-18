1df0630ff7297978b90129b398f361544f727b3d
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-10-18 12:12:51 +0800

    Update launcher.py

--- simulate/base/launcher.py
+++ simulate/base/launcher.py
@@ -1,16 +1,8 @@
 import requests
 from selenium import webdriver
-robot, session = None, None
+robot, session = None
 
 def todo(url, operator, **kw):
-    global robot, session
-    if robot is None or session is None:
-        robot = webdriver.Chrome(executable_path=r'D:\Desktop\chromedriver.exe')
-        session = requests.Session()
-        robot.implicitly_wait(2.0)
+    global robot
     robot.get(url)
     operator(kw)
-    cookies = robot.get_cookies()
-    for cookie in cookies:
-        session.cookies.set(cookie['name'], cookie['value'])
-    return robot.command_executor._url, robot.session_id
