57d53d147c792670b5a288c0367c03c90650bbdf
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-10-18 14:44:10 +0800

    Create writeData.py

---
+++ simulate/GzhuOJ/writeData.py
@@ -0,0 +1,23 @@
+import SimuateAPI
+import json
+SimuateAPI.chPathToThis(__file__)
+data = {
+    "username": "username",
+    "password": "password",
+    "problem": "1000",
+    "lang": "1",
+    "code": r"""#include<stdio.h>
+int main()
+{
+    int a, b;
+    while(~scanf("%d %d", &a, &b))
+    {
+        printf("%d\n", a + b);
+    }
+    return 0;
+}
+"""
+}
+
+with open('data.json', 'w') as dump_f:
+    json.dump(data, dump_f)
