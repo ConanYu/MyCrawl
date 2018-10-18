e21cf7cc452baf3e34089877618701cc75b5bd18
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-10-18 12:20:52 +0800

    Update launcher.py

--- simulate/base/launcher.py
+++ simulate/base/launcher.py
@@ -1,8 +1,16 @@
 import requests
 from selenium import webdriver
-robot, session = None
+robot = None
 
 def todo(url, operator, **kw):
     global robot
     robot.get(url)
     operator(kw)
+
+def load(command_executor=None, session_id=None):
+    if command_executor is None or session_id is None:
+        robot = webdriver.Chrome(executable_path=r'D:\Desktop\chromedriver.exe')
+    else:
+        robot = ReuseChrome(command_executor, session_id)
+    with open('last.log', 'w') as dump_file:
+        dump_file.write('robot.command_executor: ' + robot.command_executor._url + '\nrobot.session_id: ' + robot.session_id)
