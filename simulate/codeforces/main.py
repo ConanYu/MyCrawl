83959aeb681bdaa3479bb776b3354dfdba2bdcbe
ConanYu <33613505+ConanYu@users.noreply.github.com>
2018-10-23 13:57:01 +0800

    Update main.py

--- simulate/codeforces/main.py
+++ simulate/codeforces/main.py
@@ -8,29 +8,28 @@ sys.path.append(THIS_PATH + r'\..')
 import SimuateAPI
 robot = None
 
-@SimuateAPI.todo
-def operator_login(**kw):
+def login(**kw):
+    SimuateAPI.toUrl('http://codeforces.com/enter')
     try:
-        robot.find_element_by_xpath('//*[@id="handleOrEmail"]').send_keys(kw['username'])
-        robot.find_element_by_xpath('//*[@id="password"]').send_keys(kw['password'])
-        robot.find_element_by_xpath('//*[@id="enterForm"]/table/tbody/tr[4]/td/div[1]/input').click()
+        SimuateAPI.todo('//*[@id="handleOrEmail"]', 'send_keys', kw['username'])
+        SimuateAPI.todo('//*[@id="password"]', 'send_keys', kw['password'])
+        SimuateAPI.todo('//*[@id="enterForm"]/table/tbody/tr[4]/td/div[1]/input', 'click')
     except selenium.common.exceptions.NoSuchElementException:
         print('登录失败，可能已经登陆。')
 
-@SimuateAPI.todo
-def operator_makefriend(**kw):
+def makefriend(**kw):
     # url = 'http://codeforces.com/profile/用户名'
-    robot.find_element_by_xpath('//*[@id="pageContent"]/div[2]/div[5]/div[2]/div/h1/img[@class="addFriend friendStar" or class="friendStar addFriend"]').click()
+    SimuateAPI.toUrl(kw['url'])
+    SimuateAPI.todo('//*[@id="pageContent"]/div[2]/div[5]/div[2]/div/h1/img[@class="addFriend friendStar" or class="friendStar addFriend"]', 'click')
 
 if __name__ == '__main__':
     SimuateAPI.chPathToThis(__file__)
-    data = SimuateAPI.getData(__file__)
-    robot = SimuateAPI.load(data['command_executor'], data['session_id'])
-    print(robot)
-    operator_login(robot, url='http://codeforces.com/enter', username=data['username'], password=data['password'])
+    data = SimuateAPI.getData(THIS_PATH + r'\data.json')
+    SimuateAPI.load(data['command_executor'], data['session_id'])
+    login(username=data['username'], password=data['password'])
     time.sleep(4.0)
     for i in data['friends']:
         try:
-            operator_makefriend(robot, url=('http://codeforces.com/profile/' + i))
+            makefriend(url=('http://codeforces.com/profile/' + i))
         except selenium.common.exceptions.NoSuchElementException:
             print(i + ' 添加失败。')
